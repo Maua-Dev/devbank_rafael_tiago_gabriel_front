17c6e09769454941d8c6edb120c3bda6851b8f97
Tiago Tokugi de Albuquerque Massuda <tiagomassuda123@gmail.com>
2024-04-02 09:34:46 -0300

    estruturação da tela de Login - Tiago - Gabriel

--- src/components/pages/TelaDaConta.tsx
+++ src/components/pages/TelaDaConta.tsx
@@ -1,10 +1,17 @@
+import './TelaDaConta.css'
+
 function TelaDaConta() {
   
 
     return (
       <>
-      <header></header>
-      <textarea name="Coloque a API"></textarea>
+        <header><img src="src/img/image.webp" width={1200} className='centro' /></header>
+          <div className='centered-container'>
+            <form>
+              <input name="Coloque a API" className='input-box' />
+            </form>
+            <button> Acessar </button>
+          </div>
       </>
     )
   }
