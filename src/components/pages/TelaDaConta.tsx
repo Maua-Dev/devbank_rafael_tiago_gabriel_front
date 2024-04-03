5a3eb9d741054161e25d8c4702c9b1f1ae973235
Tiago Tokugi de Albuquerque Massuda <tiagomassuda123@gmail.com>
2024-04-03 18:58:08 -0300

    Ajustes na tela home

--- src/components/pages/TelaDaConta.tsx
+++ src/components/pages/TelaDaConta.tsx
@@ -1,9 +1,15 @@
+// import { useState } from 'react'
 import './TelaDaConta.css'
 import { Link } from "react-router-dom"
-import React from "react-router-dom"
+
 
 function TelaDaConta() {
   
+  // const [api, setApi] = useState<string>("")
+
+  // useEffect(() => {
+    
+  // }, [])
   
     return (
       <>
@@ -11,7 +17,7 @@ function TelaDaConta() {
           <div className='centered-container'>
             <div><img src="src/img/image.webp" className='centro-imagem-login' /></div>
             <form>
-              <input name="" className='input-box' />
+              <input name="" className='input-box' /*onChange={(event) => setApi(event.target.value)} */ />
             </form>
             <Link to="/home"><button className='botao-login'> Acessar </button></Link>
           </div>
--- src/components/pages/TelaInicial.tsx
+++ src/components/pages/TelaInicial.tsx
@@ -1,30 +1,41 @@
 import { Link } from "react-router-dom"
-import React from "react-router-dom"
 import "./TelaInicial.css"
+// import axios from "axios"
+// import { useEffect } from "react"
 
 function Telainicial() {
   
+  // const [name, setName] = useState<string>()
+
+  // const api = "URL context"
+
+  //const requestApi = async () => {
+  //   const response = axios.get(api)
+  //   setName(response.data.name)
+  //}
+  // useEffect(() => {
+  //    requestApi()
+  // }, [])
 
     return (
       <>
       <header>
-        
         <img src="src/img/image.webp" className='centro-imagem'/>
-        
-      
-      
       </header>
     
-
-      <Link to="/"><button name="Coloque a API">Voltar</button></Link>
       <div className="container">
 
-        <div><h2>O que deseja Fazer?</h2><input type="text" className="input-box-saldo"/></div>
+        <div>
+          <h2>O que deseja Fazer?</h2>
+          <text type="text" className="input-box-saldo"/>
+        </div>
 
-        <button className="botoes-banco">Sacar</button>
-        <button className="botoes-banco">Depositar</button>
-        <button className="botoes-banco">transações</button>
-        <input type="text" className="input-box"/>
+          <button className="botoes-banco">Sacar</button>
+          <button className="botoes-banco">Depositar</button>
+          <button className="botoes-banco">transações</button>
+          <input type="text" className="input-box"/>
+
+      <Link to="/"><button name="Coloque a API">Voltar</button></Link>
       </div>
       </>
     )
@@ -32,3 +43,38 @@ function Telainicial() {
   
   export default Telainicial
   
+
+
+
+//CONTEXT
+
+// type VariaveisGlobaisType = {
+//   api: string,
+//   nome: string,
+//   agencia: string,
+//   conta: string,
+//   credito: number,
+
+//   setApi: React.Dispatch<React.SetStateAction<string>>;
+//   setNome: React.Dispatch<React.SetStateAction<string>>;
+//   setAgencia: React.Dispatch<React.SetStateAction<string>>;
+//   setConta: React.Dispatch<React.SetStateAction<string>>;
+//   setCredito: React.Dispatch<React.SetStateAction<string>>;
+
+// } 
+// const defaultVariaveisGlobais: VariaveisGlobaisType = {
+//   api: "",
+//   nome: "",
+//   agencia: "",
+//   conta: "",
+//   credito: 0,
+
+//   setApi: () => {},
+//   setNome: () => {},
+//   setAgencia: () => {},
+//   setConta: () => {},
+//   setCredito: () => {}
+// }
+
+// export const VariaveisGlobais = createContext <VariaveisGlobaisType>(defaultVariaveisGlobais);
+
