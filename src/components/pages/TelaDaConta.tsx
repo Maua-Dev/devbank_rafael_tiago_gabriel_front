111cb217e73ad6685f02bf31bbb56b9cf68116e7
Tiago Tokugi de Albuquerque Massuda <tiagomassuda123@gmail.com>
2024-04-06 13:41:38 -0300

    Finalização da funcionalidade de validação

--- src/components/pages/TelaDaConta.tsx
+++ src/components/pages/TelaDaConta.tsx
@@ -1,26 +1,30 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import './TelaDaConta.css'
 import { ValoresDaConta } from '../../contexts/assessment-context'
+import { useNavigate } from 'react-router-dom'
 
 function TelaDaConta() {
 
   const { api, setApi } = useContext(ValoresDaConta)
+  const navigate = useNavigate()
   
   
-
   const pegar = () => {
     const chave =  "https://r2tcz6zsokynb72jb6o4ffd5nm0ryfyz.lambda-url.us-west-2.on.aws/"
-
+    
     if (api === chave){
       setApi(chave)
-      window.location.href = "/home"
+      navigate("/home")
     }  
     else {
       alert("Coloque a api correta!")
     }
   }
-
-
+  
+  useEffect(() => {
+    setApi("")
+ }, [])
+  
     return (
   
       <div className='TelaDaConta'>
@@ -28,8 +32,11 @@ function TelaDaConta() {
             <header>
               <img src="src/img/image.webp" className='centro-imagem-login' />
             </header>
-            <input id="validarApi" name="" className='input-box' onChange={(event) => setApi(event.target.value)}/>
-            <button className='botao-login' onClick={pegar} id="pegar"> Logar </button>
+            <div className='janela-input'>
+              <h1 className='instrucoes'>Coloque o link da sua conta:</h1>
+              <input id="validarApi" name="" className='input-box' onChange={(event) => setApi(event.target.value)}/>
+              <button className='botao-login' onClick={pegar} id="pegar"> Logar </button>
+            </div>
           </div>
       </div>
     )
